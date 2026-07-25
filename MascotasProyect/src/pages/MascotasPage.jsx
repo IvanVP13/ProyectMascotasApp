2f4b14187aa67fd7431350616327cda98cf48107
IvanVP13 <ivan.viscarra@inacapmail.cl>
2026-07-25 17:54:43 -0400

    feat: Se integro peticion Get en MascotasPage
    Se implemento el llamado a la API para obtener listado de mascotas y se uso el metodo .map() en MascotasList para rederizar las tarjetas con datos

--- MascotasProyect/src/pages/MascotasPage.jsx
+++ MascotasProyect/src/pages/MascotasPage.jsx
@@ -1,11 +1,31 @@
+import { useEffect, useState } from "react";
+import MascotasList from "../components/MascotasList";
+import apiMascotas from "../api/apiMascotas";
+
 function MascotasPage(){
+    const [mascotasList, setmascotaList] = useState([]);
+    //Traemos los datos con fecth
+    const fetchMascotas = async ()  => {
+        try{
+            //Peticion get
+            const response = await apiMascotas.get("mascotas/");
+            console.log(response);
+            if (response.status === 200){
+                //Guardar informacion en variable
+                setmascotaList(response.data)
+            }
+
+        }catch(error){
+            console.log(error.response);
+        };
+    }
+    useEffect(() => { fetchMascotas() }, [])
     return(
+        <article>
+            <h1>Pagina Mascotas</h1>
+            <MascotasList  lista={mascotasList}/>
 
-        <>
-            <h1>Pagina mascotas</h1>
-        </>
-    )
-    
-}
+        </article>
+    )}
 
-export default MascotasPage;
+export default MascotasPage
